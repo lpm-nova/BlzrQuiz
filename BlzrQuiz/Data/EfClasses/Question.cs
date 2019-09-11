e730f3f6082ea3796eeeae91e855e46c2ab102d0
Leon <leon.mansfield.ctr@asmr.com>
2019-09-11 08:59:39 -0400

    updated models

--- BlzrQuiz/Data/EfClasses/Question.cs
+++ BlzrQuiz/Data/EfClasses/Question.cs
@@ -4,20 +4,11 @@ namespace BlzrQuiz.Data.EfClasses
 {
     public partial class Question
     {
-        //protected Question(string text, List<string> tags, List<Answer> answers)
-        //{
-        //    Text = text ?? throw new ArgumentNullException(nameof(text));
-        //    Tags = tags ?? throw new ArgumentNullException(nameof(tags));
-        //    Answers = answers ?? throw new ArgumentNullException(nameof(answers));
-        //}
 
         public int QuestionId { get; set; }
         public string Text { get; set; }
         public List<QuestionTags> Tags { get; set; }
-        public List<QuestionAnswer> Answers { get; set; }
-
-        public int QuizId { get; set; }
-        public Quiz Quiz { get; set; }
+        public List<Answer> Answers { get; set; }
     }
 
     public enum QResult
--- BlzrQuiz/Data/EfClasses/Quiz.cs
+++ BlzrQuiz/Data/EfClasses/Quiz.cs
@@ -7,7 +7,5 @@ namespace BlzrQuiz.Data.EfClasses
         public int QuizId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<Question> Questions { get; set; }
-        public int GetScore() { return 0; }
     }
 }
