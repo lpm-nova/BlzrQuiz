27874cf670ccd7864735317309eb89b29a711e71
Leon Mansfield <chopzdcat2002@yahoo.com>
2019-10-16 17:02:59 -0400

    Updates to .net core 3

---
+++ Data/Entities/Question.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BlzrQuiz.Data.EfClasses
+{
+    public partial class Question
+    {
+        public int QuestionId { get; set; }
+        public Certification Certification { get; set; }
+        public int CertificationId { get; set; }
+        public string Text { get; set; }
+        public bool HasMultipleAnswers { get; set; }
+        
+        public virtual ICollection<Answer> Answers { get; set; }
+
+        public ICollection<QuizQuestion> QuizQuestions { get; set; }
+        //public ICollection<QuestionAnswer> QuestionAnswers { get; set; }
+    }
+
+    public enum QResult
+    {
+        NoResult,
+        Correct,
+        PartiallyCorrect,
+        Incorrect
+    }
+}
